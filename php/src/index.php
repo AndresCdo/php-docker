b6fe0f5c204399cef73a1094b74ff8f25a8db3f7
Andres Caicedo <Andrezz1997@gmail.com>
2023-04-10 18:45:40 +0200

    Update index.php

--- php/src/index.php
+++ php/src/index.php
@@ -15,7 +15,7 @@ $conn = new mysqli($host, $user, $pass);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-    echo "Connected to MySQL server successfully!";
+    echo file_get_contents("_main_.html");
 }
 ?>
 
